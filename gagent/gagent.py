24b6ab1e9be9eeb28e5370563ba4fb73844c3061
tychota <tychota@users.noreply.github.com>
2018-03-04 18:42:10 +0100

    Make agent faster

--- gagent/gagent.py
+++ gagent/gagent.py
@@ -1,4 +1,7 @@
 import random
+from typing import List
+
+import numpy as np
 
 from gagent.helper import is_point_an_eye
 from gboard.gmove import GMove
@@ -12,14 +15,29 @@ class GAgent:
 
 
 class GAgentRandom(GAgent):
+    def __init__(self):
+        self.dim = None
+        self.point_cache: List[GPoint] = []
+
+    def _update_cache(self, dim: (int, int)):
+        self.dim = dim
+        rows, cols = dim
+        self.point_cache = []
+        for r in range(1, rows + 1):
+            for c in range(1, cols + 1):
+                self.point_cache.append(GPoint(row=r, col=c))
+
     def select_move(self, game_state: GState):
-        candidates = []
-        for r in range(1, game_state.gboard.num_rows + 1):
-            for c in range(1, game_state.gboard.num_cols + 1):
-                candidate = GPoint(row=r, col=c)
-                if game_state.is_valid_move(GMove.play(candidate)) and \
-                        not is_point_an_eye(game_state.gboard, candidate, game_state.next_gplayer):
-                    candidates.append(candidate)
-        if not candidates:
-            return GMove.pass_turn()
-        return GMove.play(random.choice(candidates))
+        dim = (game_state.gboard.num_rows, game_state.gboard.num_cols)
+        if dim != self.dim:
+            self._update_cache(dim)
+
+        idx = np.arange(len(self.point_cache))
+        np.random.shuffle(idx)
+
+        for i in idx:
+            p = self.point_cache[i]
+            if game_state.is_valid_move(GMove.play(p)) and \
+                    not is_point_an_eye(game_state.gboard, p, game_state.next_gplayer):
+                return GMove.play(p)
+        return GMove.pass_turn()
