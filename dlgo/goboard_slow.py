dd38f5f9173f85d57a45f2f33a73f27289d24b60
tychota <tychota@users.noreply.github.com>
2019-01-20 20:25:02 +0100

    feat: add go_string class

--- dlgo/goboard_slow.py
+++ dlgo/goboard_slow.py
@@ -1,4 +1,5 @@
 import copy
+from typing import List
 from dlgo.gotypes import Player, Point
 
 
@@ -21,3 +22,31 @@ class Move():
     @classmethod
     def resign(cls):
         return Move(is_resign=True)
+
+
+class GoString():
+    def __init__(self, color: Player, stones: List[Point], liberties: List[Point]):
+        self.color = color
+        self.stones = set(stones)
+        self.liberties = set(liberties)
+
+    def remvove_liberty(self, point):
+        self.liberties.remove(point)
+
+    def add_liberty(self, point):
+        self.liberties.add(point)
+
+    def merged_with(self, go_string: GoString):
+        assert go_string.color == self.color
+        combined_stones = self.stones | go_string.stones
+        combined_liberties = (self.liberties | go_string.liberties) - combined_stones
+        return GoString(self.color, combined_stones, combined_liberties)
+
+    @property
+    def num_liberties(self):
+        return len(self.liberties)
+
+    def __eq__(self, other: GoString):
+        if not isinstance(other, GoString):
+            return False
+        return self.color == other.color and self.stones == other.stones and self.liberties == other.liberties
