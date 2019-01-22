6c685bdcfc60c0cf2f228b7d8b1c7e084a223ca6
tychota <tychota@users.noreply.github.com>
2019-01-23 00:25:17 +0100

    feat: add generator to process data from KGS

---
+++ load.py
@@ -0,0 +1,9 @@
+from dlgo.data.parallel_procesor import GoDataProcessor
+
+processor = GoDataProcessor(encoder='oneplane')
+generator = processor.load_go_data('train', 10000, use_generator=True)
+
+print(generator.get_num_samples())
+generator = generator.generate(batch_size=10)
+
+X, y = next(generator)
