b6f00a36299f7b3ea18f1288205664dc47c0cbcf
tychota <tychota@users.noreply.github.com>
2019-01-26 12:12:51 +0100

    feat: use agent in bot vs bot

--- bot_vs_bot.py
+++ bot_vs_bot.py
@@ -1,27 +1,32 @@
 from __future__ import print_function
 
-from dlgo.agent.mcts import MCTSAgent
-# from dlgo.agent.naive import RandomBot
+import h5py
+
+from dlgo.agent.naive import RandomBot
+from dlgo.agent.predict import load_prediction_agent
 from dlgo.goboard_fast import GameState
 from dlgo.gotypes import Player
+from dlgo.scoring import compute_game_result
 from dlgo.utils import print_board, print_move
 
 
 def main():
-    board_size = 9
-    game = GameState.new_game(board_size)
+    board_size = 19
+    game_state = GameState.new_game(board_size)
     bots = {
-        Player.black: MCTSAgent(5000, 0.8),
-        Player.white: MCTSAgent(5000, 0.8),
+        Player.black: load_prediction_agent(h5py.File("./agents/deep_bot.h5", "r")),
+        Player.white: RandomBot(),
     }
-    while not game.is_over():
+    while not game_state.is_over():
         # time.sleep(0.3)
 
         # print(chr(27) + "[2J")
-        print_board(game.board)
-        bot_move = bots[game.next_player].select_move(game)
-        print_move(game.next_player, bot_move)
-        game = game.apply_move(bot_move)
+        print_board(game_state.board)
+        bot_move = bots[game_state.next_player].select_move(game_state)
+        print_move(game_state.next_player, bot_move)
+        game_state = game_state.apply_move(bot_move)
+    print(game_state.winner())
+    print(compute_game_result(game_state))
 
 
 if __name__ == '__main__':
