96606a1e557bf20e4eb88e137f229ea2da1140e7
tychota <tychota@users.noreply.github.com>
2019-01-27 03:18:31 +0100

    feat: add gtp command / response capacities

---
+++ dlgo/gtp/board.py
@@ -0,0 +1,20 @@
+from dlgo.gotypes import Point
+from dlgo.goboard_fast import Move
+
+__all__ = [
+    'coords_to_gtp_position',
+    'gtp_position_to_coords',
+]
+
+COLS = 'ABCDEFGHJKLMNOPQRST'
+
+
+def coords_to_gtp_position(move):
+    point = move.point
+    return COLS[point.col - 1] + str(point.row)
+
+
+def gtp_position_to_coords(gtp_position):
+    col_str, row_str = gtp_position[0], gtp_position[1:]
+    point = Point(int(row_str), COLS.find(col_str.upper()) + 1)
+    return Move(point)
